82e9218868f180e81d8933924534f48e7d16b81a
ChengHan <wow0422796353@gmail.com>
2024-06-03 23:57:02 +0800

    更改邏輯腳位 - mqtt驅動之程式碼

--- README.md
+++ README.md
@@ -5,3 +5,11 @@
 # NodeRed程式碼
 架設流程=>npm install -g --unsafe-perm node-red
 啟動指令=>node-red -u {你的.node-red資料夾path}
+
+套件要自行下載:
+node-red
+node-red-contrib-moment
+node-red-dashboard
+node-red-node-mysql
+
+流程圖都得匯出 換環境時都得再匯入
--- VehicleControl/VehicleControl.ino
+++ VehicleControl/VehicleControl.ino
@@ -5,28 +5,32 @@
 
 WiFiClient espClient;
 PubSubClient client(espClient);
-
+// wifi
 const char* ssid = "BIRC";
 const char* password = "D@x6=720131224";
-const char* mqtt_server = "140.131.115.152";
+// mqtt
+const char* mqtt_server = "broker.mqttgo.io";
 const int mqtt_port = 1883;
 
-// L298N
-const int ENA = 15; // 黑 
-const int ENB = 2;  // 白
-const int IN1 = 0;  // 綠
-const int IN2 = 4;  // 藍
-const int IN3 = 5;  // 紫
-const int IN4 = 13; // 灰
+// L298N 藍紫灰白黑棕
+const int ENA = 15; // 藍 
+const int IN1 = 2;  // 
+const int IN2 = 0;  // 藍
+const int IN3 = 4;  // 紫
+const int IN4 = 16; // 灰
+const int ENB = 17;  // 白
 // RFID 偵測範圍大約 上及下的 1.5公分
-const int MOSI_PIN = 23; // 橘
-const int MISO_PIN = 19; // 紅
-const int SCK_PIN = 18;  // 白
-const int RST_PIN = 32;  // 黃
-const int SDA_PIN = 21;  // 銀 VCC=黑 GND=綜
-// 循跡
-const int rightTrackPin = 39;
-const int leftTrackPin = 36;
+// 由左到右
+// 接在esp的順序 綠黃橘紫棕
+// 接在rfid的順序   
+const int SDA_PIN = 21;  // 綠              
+const int SCK_PIN = 18;  // 固定腳位 紫
+const int MOSI_PIN = 23; // 固定腳位 橘
+const int MISO_PIN = 19; // 固定腳位 藍
+const int RST_PIN = 22;  // 黃
+// 循跡 D0
+const int rightTrackPin = 34;
+const int leftTrackPin = 35;
 // 秤重
 const int DT_PIN = 14;
 const int W_SCK_PIN = 12;
@@ -42,28 +46,26 @@ enum MotorDirection {
   STOP
 };
 
-// void setup_wifi() {
-//   delay(10);
-//   Serial.println("Connecting to WiFi..");
-//   WiFi.begin(ssid, password);
+void setup_wifi() {
+  delay(10);
+  Serial.println("Connecting to WiFi..");
+  WiFi.begin(ssid, password);
 
-//   while (WiFi.status() != WL_CONNECTED) {
-//     delay(500);
-//     Serial.print(".");
-//   }
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+  }
 
-//   Serial.println("WiFi connected");
-//   Serial.println("IP address: ");
-//   Serial.println(WiFi.localIP());
-// }
+  Serial.println("WiFi connected");
+  Serial.println(WiFi.localIP());
+}
 
-// void setup_mqtt() {
-//   client.setServer(mqtt_server, mqtt_port);
-//   client.setCallback(callback);
-// }
+void setup_mqtt() {
+  client.setServer(mqtt_server, mqtt_port);
+  client.setCallback(callback);
+  client.subscribe("ntubimd/delivering");
+}
 
 void setup_unit() {
-  // pinMode(avoidancePin, INPUT);
   pinMode(rightTrackPin, INPUT);
   pinMode(leftTrackPin, INPUT);
   pinMode(ENA, OUTPUT);
@@ -75,34 +77,31 @@ void setup_unit() {
   analogWrite(ENA, 120);
   analogWrite(ENB, 120);
   controlMotors(STOP);
-  // SPI.begin();                     // 初始化 SPI 介面
-  // mfrc522.PCD_Init();              // 初始化 MFRC522
-  // Serial.println("請將 RFID 標籤靠近讀取器...");
+  SPI.begin();
+  mfrc522.PCD_Init();
 }
 
-// void callback(char* topic, byte* message, unsigned int length) {
-//   Serial.print("Message arrived on topic: ");
-//   Serial.print(topic);
-//   Serial.print(". Message: ");
-//   String messageTemp;
-  
-//   for (int i = 0; i < length; i++) {
-//     messageTemp += (char)message[i];
-//   }
-//   Serial.println(messageTemp);
+void callback(char* topic, byte* message, unsigned int length) {
+  String messageTemp;
+  for (int i = 0; i < length; i++) {
+    messageTemp += (char)message[i];
+  }
 
-//   if (messageTemp == "FORWARD") {
-//     controlMotors(FORWARD);
-//   } else if (messageTemp == "BACKWARD") {
-//     controlMotors(BACKWARD);
-//   } else if (messageTemp == "LEFT") {
-//     controlMotors(TURN_LEFT);
-//   } else if (messageTemp == "RIGHT") {
-//     controlMotors(TURN_RIGHT);
-//   }
-//   delay(1000); // 動作持續 1 秒
-//   controlMotors(STOP); // 停止馬達
-// }
+  Serial.print("監聽主題: ");
+  Serial.println(topic);
+  Serial.print("要感應的卡號: ");
+  Serial.println(messageTemp);
+
+  if (String(topic) == "ntubimd/delivering/order") {
+    String sensor_card = delieveryUtilGetCardCode(messageTemp);
+    client.publish("ntubimd/already/delivery/order", sensor_card.c_str());
+  }
+
+  if (String(topic) == "ntubimd/delivering/medicine") {
+    String sensor_card = delieveryUtilGetCardCode(messageTemp);
+    client.publish("ntubimd/already/delivery/medicine", sensor_card.c_str());    
+  }
+}
 
 void reconnect() {
   controlMotors(STOP);
@@ -127,8 +126,36 @@ void reconnect() {
 void setup() {
   Serial.begin(9600);
   setup_unit();
-  // setup_wifi();
-  // setup_mqtt();
+  setup_wifi();
+  setup_mqtt();
+}
+
+void loop() {
+  RFID_listening()
+}
+
+String delieveryUtilGetCardCode(String card_code){
+  String sensor_card = RFID_listening();
+  controlMotorsByBlackLine();
+  if (sensor_card != card_code) {
+    controlMotors(STOP);
+  }
+  return sensor_card;
+}
+
+void controlMorosByBlackLine() {
+  int leftSensorValue = digitalRead(leftTrackPin);
+  int rightSensorValue = digitalRead(rightTrackPin);
+
+  if (leftSensorValue == 0 && rightSensorValue == 0) {
+    controlMotors(FORWARD);
+  } else if (leftSensorValue == 1 && rightSensorValue == 0) {
+    controlMotors(TURN_LEFT);
+  } else if (leftSensorValue == 0 && rightSensorValue == 1) {
+    controlMotors(TURN_RIGHT);
+  } else {
+    controlMotors(BACKWARD);
+  }
 }
 
 void controlMotors(MotorDirection direction) {
@@ -166,65 +193,18 @@ void controlMotors(MotorDirection direction) {
     }
 }
 
-// void RFID_listening() {
-//   if (mfrc522.PICC_IsNewCardPresent() && mfrc522.PICC_ReadCardSerial()) {
-//     Serial.print("卡片 UID:");
-//     unsigned long uidDec = 0;  // 用來儲存十進制的UID
-
-//     // 打印出十六進制 UID 並轉換為十進制
-//     for (byte i = 0; i < mfrc522.uid.size; i++) {
-//       Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
-//       Serial.print(mfrc522.uid.uidByte[i], HEX);
-//       uidDec = uidDec * 256 + mfrc522.uid.uidByte[i];  // 將十六進制轉換為十進制
-//     }
-//     Serial.println();
-
-//     // 打印十進制 UID
-//     Serial.print("卡片 UID (十進制): ");
-//     Serial.println(uidDec);
-
-//     // 讓讀取器準備下一次讀取
-//     mfrc522.PICC_HaltA();
-//   }
-// }
-
-void loop() {
-  // RFID_listening()
-  // int leftSensorValue = digitalRead(leftTrackPin);
-  // int rightSensorValue = digitalRead(rightTrackPin);
-
-  // if (leftSensorValue == 0 && rightSensorValue == 0) {
-  //     // 两侧传感器均检测到白色，前进
-  //     controlMotors(FORWARD);  
-  //     Serial.println("白白全亮前進");
-  // } else if (leftSensorValue == 1 && rightSensorValue == 0) {
-  //     // 左侧传感器检测到黑色，右侧检测到白色，左转
-  //     controlMotors(TURN_LEFT);
-  //     Serial.println("黑白左轉");
-  // } else if (leftSensorValue == 0 && rightSensorValue == 1) {
-  //     // 左侧传感器检测到白色，右侧检测到黑色，右转
-  //     controlMotors(TURN_RIGHT);
-  //     Serial.println("白黑右轉");
-
-  // } else {
-  //     // 其他情况（例如两侧都是黑色），可以选择停止或者定义其他行为
-  //     controlMotors(BACKWARD);
-  //     Serial.println("黑黑不亮停下");
-  // }
-  controlMotors(FORWARD);
-  delay(3000);
-  controlMotors(BACKWARD);
-  delay(3000);
+String RFID_listening() {
+  String uidHex = "";
+  if (mfrc522.PICC_IsNewCardPresent() && mfrc522.PICC_ReadCardSerial()) {
+    for (byte i = 0; i < mfrc522.uid.size; i++) {
+      if (mfrc522.uid.uidByte[i] < 0x10) {
+        uidHex += " 0";
+      } else {
+        uidHex += " ";
+      }
+      uidHex += String(mfrc522.uid.uidByte[i], HEX);
+    }
+    mfrc522.PICC_HaltA();
+  }
+  return uidHex;
 }
-
-// void getAvoidanceValue() {
-//   int avoidanceValue = digitalRead(avoidancePin);
-//   String sensorString = "";
-
-//   if (avoidanceValue == LOW) {
-//     Serial.println("Obstacle detected!");
-//   } else {
-//     Serial.println("No obstacle detected.");
-//   }
-//   delay(200);
-// }
